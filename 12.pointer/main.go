82b95be63581c6e1f1ec401d842f4fdfc5992efd
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-09-29 22:27:28 +0900

    add comments

--- 12.pointer/main.go
+++ 12.pointer/main.go
@@ -2,20 +2,37 @@ package main
 
 import "fmt"
 
-func one(x *int) {
-	*x = 1
-}
-
 func main() {
+	// 変数nに数値100を格納
 	var n int = 100
-	one(&n)
-	fmt.Println(n)
 
-	fmt.Println(&n)
+	// 変数nを出力
+	fmt.Println(n) // 100が出力される
 
+	// &を先につけることで変数があるメモリのアドレスを指定する
+	fmt.Println(&n) // メモリのアドレスが表示される
+
+	// 変数pに*でintのポインタ型にして、&nを格納する
 	var p *int = &n
 
+	// メモリのアドレスが表示される
 	fmt.Println(p)
 
-	fmt.Println(*p)
+	// *pにすることで中身を参照する
+	fmt.Println(*p) // 100が表示される
+
+	// &をつけて、nのアドレスを渡し、one関数は*がついてるので、アドレスの中身を書き換える
+	one(&n)
+	fmt.Println(n)
+}
+
+// *をつけずにxの値を変更しても、中身に影響はない
+// func one(x int) {
+// 	x = 1
+// }
+
+// *をつけて中身を参照することで、xの値が１になる
+func one(x *int) {
+	// xの実態の中に1を格納する。dereferenceと呼ばれる
+	*x = 1
 }
