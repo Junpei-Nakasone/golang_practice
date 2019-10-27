08ed03adeda2795bb911a03ce162fdaf0a960f78
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-10-27 11:22:21 +0900

    add comment

--- 19.DefaultSelection/main.go
+++ 19.DefaultSelection/main.go
@@ -6,13 +6,21 @@ import (
 )
 
 func main() {
+	// channelを返す
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(500 * time.Millisecond)
 
 	for {
 		select {
+
+		// tick channelを受信したcase, 値は何も受け取らない
 		case <-tick:
 			fmt.Println("tick.")
+		// tick channelを変数に格納することもできる,
+		// 下記のように書くと文字列だけじゃなくchannelが持つtimeのデータも表示する
+		// case t := <-tick:
+		// fmt.Println("tick.", t)
+
 		case <-boom:
 			fmt.Println("BOOM!")
 			return
